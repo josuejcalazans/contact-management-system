47f541a2702e4fe837a5453f3b51ff2bede40d99
Josue J Calazans <josue.calazans07@gmail.com>
2025-03-13 13:33:19 -0300

    feat: :sparkles: Bind ContactRepositoryInterface to ContactRepository in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\ContactRepositoryInterface;
+use App\Repositories\ContactRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            ContactRepositoryInterface::class,
+            ContactRepository::class
+        );
     }
 
     /**
