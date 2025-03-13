63e1b291072c96fe03a0d76ef6415f8ea47c5ee6
Josue J Calazans <josue.calazans07@gmail.com>
2025-03-13 13:27:10 -0300

    refactor: :recycle: Update ContactRequest with validation rules and phone formatting

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -6,15 +6,25 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|min:3',
+            'email' => 'required|email|unique:contacts,email',
+            'phone' => 'required|string|min:10',
         ];
+    }
+    protected function prepareForValidation()
+    {
+        if ($this->has('phone')) {
+            $this->merge([
+                'phone' => preg_replace('/\D/', '', $this->input('phone')),
+            ]);
+        }
     }
 }
